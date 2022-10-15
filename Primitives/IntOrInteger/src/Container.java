49e876da4a4f5cb9454dc8b6e46b9ca3d44d4edd
Alena Penyajina <penyajinaalena@gmail.com>
2022-10-16 00:13:40 +0600

    Homework Primitives

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,11 +1,32 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0; //boxing
 
     public void addCount(int value) {
-        count = count + value;
-    }
+        count = count+value;
+    } //unboxing
 
     public int getCount() {
+        System.out.print("Проверка первого задания: ");
         return count;
+
+    }
+
+    //1040 - а 1103 - я  1105 - ё 1025 - Ё
+    public void CharFinder() {
+        System.out.println("Второе задание: ");
+        for (int i = 0; i < 65536; i++) {
+            char c = (char) i;
+            if (i >= 1040 && i <= 1103) {
+                if (i == 1045) {
+                    System.out.println(i+" - "+c);
+                    c = 1025;
+                }
+                if (i == 1077) {
+                    System.out.println(i+" - "+c);
+                    c = 1105;
+                }
+                System.out.println(i+" - "+c);
+            }
+        }
     }
 }
--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
+        Container container1 = new Container();
+        container1.addCount(5235);
+        container = container1;
+        System.out.println(container.getCount());
+        container.CharFinder();
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
